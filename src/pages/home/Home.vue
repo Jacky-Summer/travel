d28598190b3509f4995f9bea40cca0aa1040533b
Jacky-Summer <1106619871@qq.com>
2019-10-26 23:22:46 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>home</div>
+    <home-header></home-header>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题</div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/variables.styl'
+    .header
+        display flex
+        line-height $headerHeight
+        background $bgColor
+        color #fff
+        .header-left
+            float left
+            width .64rem
+            .back-icon
+                text-align: center
+                font-size: .4rem
+        .header-input
+            flex 1
+            height .64rem
+            line-height .64rem
+            margin-left .2rem
+            margin-top .12rem
+            padding-left: .2rem
+            background: #fff
+            border-radius: .1rem
+            color: #ccc
+        .header-right
+            float: right
+            text-align: center
+            color: #fff
+            min-width: 1.04rem
+            padding: 0 .1rem
+            .arrow-icon
+                margin-left: -.04rem
+                font-size: .24rem
+</style>
