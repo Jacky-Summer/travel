b660766a489a16a116a02cf45fc66d8e407f399b
Jacky-Summer <1106619871@qq.com>
2019-11-12 20:19:05 +0800

    detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+          <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '双人票',
+          children: [{
+            title: '情侣双人票'
+          }]
+        }, {
+          title: '三人票'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '学生票'
+      }, {
+        title: '老人票'
+      }]
+    }
+  },
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
